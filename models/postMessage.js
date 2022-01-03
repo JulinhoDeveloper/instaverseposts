733d49e0d48833c39fbc679fb2edc5f4c948dfd7
JulioDevFullStack <juliodevfullstack@gmail.com>
2022-01-03 19:03:12 -0300

    models de posts e users

---
+++ models/postMessage.js
@@ -0,0 +1,20 @@
+const mongoose = ('mongoose');
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+   title: String,
+   message: String,
+   name: String,
+   creator: String,
+   tags: [String],
+   selectedFile: String,
+   likes: {
+      type: [String],
+      default: []
+   },
+   createdAt: {
+      type: Date,
+      default: new Date()
+   }
+})
+module.exports=mongoose.model("PostMessage", postSchema);
