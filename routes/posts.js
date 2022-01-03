2546c6a8b6c436f15bfae2938f2ce6f20d344b27
JulioDevFullStack <juliodevfullstack@gmail.com>
2022-01-03 19:06:15 -0300

    construindo as rotas dos posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,14 @@
 const express = require ('express');
 
-
+const { getPosts, createPost, updatePost, deletePost, likePost } = require ('../controllers/posts.js');
 
 const router = express.Router();
+
+router.get('/', getPosts);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost)
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 
 module.exports = router;
